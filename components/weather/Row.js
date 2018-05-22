86a42c5723c97981207ddcbccbe0a71f8fdf063d
BrunoCambianica <bruno.cambianica@gmail.com>
2018-05-22 18:56:43 +0200

    correction températures ajout  heures affichage a modifier

--- components/weather/Row.js
+++ components/weather/Row.js
@@ -10,7 +10,7 @@ export default class Row extends React.Component {
 
     constructor(props) {
         super(props)
-        let {width} = Dimensions.get('window')
+        let { width } = Dimensions.get('window')
         this.state = {
             widthScreen: width
         }
@@ -26,39 +26,52 @@ export default class Row extends React.Component {
     day() {
         let day = moment(this.props.day.dt * 1000).format('ddd')
         return (
-            <Text style={[style.white, style.bold]}> {day.toUpperCase()} </Text>
+            <Text style={[style.white, style.bold]}>
+                {day.toUpperCase()}
+            </Text>
         )
     }
 
     date() {
         let day = moment(this.props.day.dt * 1000).format('DD/MM')
         return (
-            <Text style={style.white}> {day} </Text>
+            <Text style={style.white}>
+                {day}
+            </Text>
         )
     }
 
-    icon(widthI = 100, heightI = 100) {
+    hour() {
+        let hour = moment(this.props.day.dt * 1000).format('LT')
+        return (
+            <Text style={[style.white, style.hour]}>
+                {hour}
+            </Text>
+        )
+    }
+
+    icon(widthI = 60, heightI = 60) {
         const type = this.props.day.weather[0].main.toLowerCase()
         let image
         switch (type) {
             case 'clear':
-                image = require('../../images/sunny.png')
+                image = require('../../images/png/sunny.png')
                 break;
             case 'rain':
-                image = require('../../images/rainy.png')
+                image = require('../../images/png/rain.png')
                 break;
             case 'snow':
-                image = require('../../images/snow.png')
+                image = require('../../images/png/snow.png')
                 break;
             case 'clouds':
-                image = require('../../images/cloudy.png')
+                image = require('../../images/png/cloudy.png')
                 break;
 
             default:
                 break;
         }
         return (
-            <Image source={image} style={{ width: widthI, height: heightI }} />
+            <Image source={image} style={{ width: widthI, height: heightI, justifyContent: 'space-between' }} />
         )
     }
 
@@ -68,11 +81,15 @@ export default class Row extends React.Component {
                 <Effects delay={this.props.index * 50}>
                     <View style={[style.view]}>
                         {this.icon(widthI = this.state.widthScreen)}
-                        <View style={[style.first, {position: 'absolute', top: 0, left: 0}]}>
-                            <Text style={style.first}> {this.day()} {this.date()}</Text>
-                            <Text style={[style.temp, style.first]}> {this.props.day.main.temp}  °C</Text>
+                        <View style={[style.first, { position: 'absolute', top: 0, left: 0 }]}>
+                            <Text style={style.first}>
+                                {this.day()} {this.date()} {this.hour()}
+                            </Text>
+                            <Text style={[style.temp, style.first]}>
+                                {Math.round(this.props.day.main.temp - 273.15)}  °C
+                            </Text>
                         </View>
-                        
+
                     </View>
                 </Effects>
             )
@@ -80,13 +97,24 @@ export default class Row extends React.Component {
         else {
             return (
                 <Effects delay={this.props.index * 50}>
-                    <View style={style.view}>
+                    <View style={[style.view]}>
                         <View style={{ flex: 1, flexDirection: 'row' }}>
-                            <Text> {this.day()} {this.date()}</Text>
-                            <Text style={{ marginLeft: 20 }}> {this.icon()}</Text>
+                            <View style={{ flex: 1, flexDirection: 'column' }}>
+                                <Text>
+                                    {this.day()} {this.date()}
+                                </Text>
+                                <Text>
+                                    {this.hour()}
+                                </Text>
+                            </View>
+                            {this.icon()}
+                            <Text style={style.temp}>
+                                {Math.round(this.props.day.main.temp - 273.15)}  °C
+                            </Text>
+
                         </View>
 
-                        <Text style={style.temp}> {this.props.day.main.temp}  °C</Text>
+
                     </View>
                 </Effects>
             )
@@ -95,6 +123,10 @@ export default class Row extends React.Component {
 }
 
 const style = StyleSheet.create({
+    hour: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
     white: {
         color: 'white'
     },
@@ -102,7 +134,7 @@ const style = StyleSheet.create({
         fontWeight: 'bold'
     },
     view: {
-        backgroundColor: globalStyle.color,
+        backgroundColor: '#999999',
         borderWidth: 0,
         borderBottomWidth: 1,
         borderBottomColor: 'lightblue',
@@ -113,7 +145,7 @@ const style = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center'
     },
-    first :{
+    first: {
         fontSize: 30,
         marginLeft: 50
     },
