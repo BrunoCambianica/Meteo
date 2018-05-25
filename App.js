56a19d7ef6fdf62885b36ba244663ca2e99571ed
BrunoBTS2B <bruno.cambianica@gmail.com>
2018-05-25 09:00:59 +0200

    le commit bien relou

--- App.js
+++ App.js
@@ -1,31 +1,36 @@
 import React from 'react'
 import { StyleSheet, Text, View, StatusBar } from 'react-native'
-import { TabNavigator } from 'react-navigation'
+import { TabNavigator, StackNavigator, StackNavigatorConfig } from 'react-navigation'
 
 import About from './components/About'
 import Search from './components/Search'
 
- const Tabs = TabNavigator({
+// const Tabs = TabNavigator({
+//   Search: { screen: Search },
+//   About: { screen: About },
+// },
+//   {
+//     tabBarPosition: 'bottom',
+//     swipeEnabled: true,
+//     animationEnabled: true,
+//     backgroundColor: '#4d4dff',
+//     tabBarOptions: {
+
+//       showIcon: false,
+//       showLabel: true,
+//       indicatorStyle: {
+//         backgroundColor: 'white',
+//         height: 3
+//       }
+
+//     }
+
+//   })
+
+const Tabs = createStackNavigator({
   Search: { screen: Search },
   About: { screen: About },
-},
-  {
-    tabBarPosition: 'bottom',
-    swipeEnabled: true,
-    animationEnabled: true,
-    backgroundColor: '#4d4dff',
-    tabBarOptions: {
-
-      showIcon: false,
-      showLabel: true,
-      indicatorStyle: {
-        backgroundColor: 'white',
-        height: 3
-      }
-
-    }
-
-  })
+})
 
 export default class App extends React.Component {
   render() {
